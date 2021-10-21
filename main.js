f15d6fc5d73b599229f226e74ac3e807f1235e02
arghadwip23 <arghadwip23@gmail.com>
2021-10-21 20:45:25 +0530

    update

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 console.log('Hello World!');
+var buttonToOpenMenubar = document.getElementById("menubar_open");
+var buttonToCloseMenubar = document.getElementById("menubar_close");
+var menubar = document.getElementById("navbar");
+
+
+// function open menu bar , for which I will use event listener
+buttonToOpenMenubar.addEventListener("click" , ()=>{
+  menubar.style.display="flex";
+})
+
+
+//functions to close the menubar
+buttonToCloseMenubar.addEventListener("click", () =>{
+  menubar.style.display="none";
+})
