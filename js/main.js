e89302131f8dba2ab150a825af29fa4e9a65e78c
arghadwip23 <arghadwip23@gmail.com>
2021-10-25 07:42:09 +0530

    a lot of update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
+//import modules from other js filles
+import { animations } from "../js/firstHeading.js";
+import { isScrolledIntoView } from "../js/isVisible.js";
+
 console.log('Hello World!');
+
+
+
+//select the DOM element
 var buttonToOpenMenubar = document.getElementById("menubar_open");
 var buttonToCloseMenubar = document.getElementById("menubar_close");
 var menubar = document.getElementById("navbar");
+var screen = document.getElementById("fs");
+
+animations();
+document.addEventListener("scroll" ,  ()=>{
+  if (isScrolledIntoView(screen)) {
+    animations();
+  }
+})
+
+
+
 
 
 // function open menu bar , for which I will use event listener
